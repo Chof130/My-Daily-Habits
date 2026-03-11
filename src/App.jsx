7491b4f0f40338b350f8f74eba597629e1e4f066
Márcio H <marcinho_mh@hotmail.com>
2026-03-11 19:09:54 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Footer from './components/Footer'
+import './App.css'
+
+
+function App(){
+  return(
+    <div>
+      <h1>My Daily Habits</h1>
+        <p>Gerencie seus hábitos diários de forma simples e visual</p>
+    <Footer/>
+    </div>
+        )
+              }
+
+export default App
+
+
+
+
